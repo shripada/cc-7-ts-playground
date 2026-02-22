bc148b26b19cea0f41e3bb05ed2e59f26c90c4f3
shripada <shripada.hebbar@codecraft.co.in>
2026-02-22 21:09:35 +0530

    classes concluded

--- 7.classes/classes.ts
+++ 7.classes/classes.ts
@@ -134,4 +134,63 @@ class Album {
 const album = new Album('ZZ', 'Michael', 1988);
 album.title = 'Thriller'; // not allowed as title is readonly
 
-// ! Exercise. Fix the issue in event-test app, where there is a bug in handling button click and updating count value.
+// ! Exercise. Fix the issue in event-test app in 7.classes/event-test, where there is a bug in handling button click and updating count value.
+
+// * Class inheritence
+interface Point {
+  x: number;
+  y: number;
+}
+abstract class Shape {
+  // A class can be marked as abstract, if you dont want explicit instance of this class be ever created.
+  private origin: Point; // private is only accessible within the class.
+
+  area(): number {
+    return 0;
+  }
+
+  constructor() {
+    this.origin = { x: 0, y: 0 };
+  }
+}
+
+class Rectangle extends Shape {
+  private width: number;
+  private height: number;
+
+  area() {
+    // Implicite overrides can be warned using "noImplicitOverride": true, setting in tsconfig.json
+    // Always use override keyword to ensure safe overrides, in case base method is renamed, we will get an error.
+    return this.width * this.height;
+  }
+
+  constructor(width: number, height: number) {
+    super();
+    this.width = width;
+    this.height = height;
+  }
+  // ! exercise, rewrite the constructor where the vars width, and height will be auto created.
+
+  // * Getters
+  get w() {
+    return this.width;
+  }
+
+  get h() {
+    return this.height;
+  }
+
+  //* Setters
+  set w(width: number) {
+    this.width = width;
+  }
+  set h(height: number) {
+    this.height = height;
+  }
+}
+
+const rectangle = new Rectangle(10, 22);
+rectangle.w = 15;
+rectangle.h = rectangle.w + 10; // Setters and getters usage
+
+//! Exercise. Implement Circle class. It should override from Shape. Implement getter to access its radius.  Then create an Array of circles and rectangles. Find the item in the array that has least area.
